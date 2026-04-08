dad623acd0cd051a58ca0af8c54a977ec8924c7b
halaidarous25-droid <halaidarous25@gmail.com>
2026-04-08 15:06:52 +0300

    Change Supabase key to new value
    Updated Supabase key for API access.

--- supabase.js
+++ supabase.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL = https://rfvbzeopxusnmumazipq.supabase.co;
-const SUPABASE_KEY = sb_publishable_bPHvJG0OJk3FuJOBX_0aJQ_fE8KzTEx;
+const SUPABASE_KEY = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJmdmJ6ZW9weHVzbm11bWF6aXBxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU2NDU3OTgsImV4cCI6MjA5MTIyMTc5OH0.7es4Q8zWPR8uPBZwXzbXzqpq6DRqXLDEdQM5HGIl9kM;
 
 const supabase = window.supabase.createClient(
   SUPABASE_URL,
