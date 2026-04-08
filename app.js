50164ddce8cff94f9f89778bc271eb3bdbe2ed67
halaidarous25-droid <halaidarous25@gmail.com>
2026-04-08 19:26:34 +0300

    Update app.js

--- app.js
+++ app.js
@@ -85,13 +85,13 @@ async function createRequest() {
 }
 window.createRequest = createRequest;
 window.login = login;
-document.addEventListener("DOMContentLoaded", () => {
-  const btn = document.getElementById("submitBtn");
+const btn = document.getElementById("submitBtn");
 
-  if (btn) {
-    btn.addEventListener("click", () => {
-      console.log("BUTTON CLICKED");
-      createRequest();
-    });
+if (btn) {
+  btn.addEventListener("click", () => {
+    console.log("BUTTON CLICKED");
+    createRequest();
+  });
+}
   }
 });
