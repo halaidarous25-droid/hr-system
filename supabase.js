2cd189704493ef6be97170b9e781c06e877f0716
halaidarous25-droid <halaidarous25@gmail.com>
2026-04-08 15:07:52 +0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL = https://rfvbzeopxusnmumazipq.supabase.co;
-const SUPABASE_KEY = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJmdmJ6ZW9weHVzbm11bWF6aXBxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU2NDU3OTgsImV4cCI6MjA5MTIyMTc5OH0.7es4Q8zWPR8uPBZwXzbXzqpq6DRqXLDEdQM5HGIl9kM;
+const SUPABASE_KEY = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9;
 
 const supabase = window.supabase.createClient(
   SUPABASE_URL,
